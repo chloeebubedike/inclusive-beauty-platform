27ee8c5f7e3637f5f982e5f77b2aa426263c32b8
Chloe <chloeebubedike@yahoo.com>
2025-09-10 14:14:05 +0100

    Added python backend

---
+++ inclusive-beauty-platform/src/pages/Home.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import { ServiceCard } from '../components/ServiceCard';
+import React from 'react';
+
+interface Service {
+  name: string;
+  price: number;
+  provider: string;
+  isNew: boolean;
+}
+
+interface HomeProps {
+  services?: Service[];
+}
+
+const Home = ({ services }: HomeProps) => {
+  return (
+    <div className='flex flex-col gap-5 mt-14 mb-14'>
+      {services?.map((service: Service) => (
+        <ServiceCard
+          serviceName={service.name}
+          price={service.price}
+          serviceProvider={service.provider}
+          isServiceNew={service.isNew}
+        />
+      ))}
+    </div>
+  );
+};
+export default Home;
